f9613f7d24abacc99093b3bbdea7b7514c6c675e
vvboy <1285348378@qq.com>
2021-01-13 00:01:30 +0800

    callback中间层不需要重新实现，可以直接把第一层实现的回调函数往后传即可

---
+++ components/category-grid/index.js
@@ -0,0 +1,30 @@
+// components/category-grid/index.js
+Component({
+  /**
+   * 主要定义外部属性
+   * 组件的属性列表
+   */
+  properties: {
+    // grid: {
+    //   type:Array,
+    //   value:[]
+    // }
+    // 因为初始值是默认值，就不用上面那样
+    grid:Array
+  },
+
+  /**
+   * 放置内部属性
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- model/Theme.js
+++ model/Theme.js
@@ -1,26 +1,20 @@
-/**
- * @作者 sun
- * @创建时间 2020/10/27 0:40
- */
-
 'use strict'
 // 业务对象
 // theme,banner,sku,spu,address,user等是业务对象，home不是
 
-import {config} from "../config/config"
 import {Http} from "../utils/http"
 
 class Theme {
+
+  static locationA = 't-1'
+
   // 为什么要static，后面有不用静态的方法就知道了
   // 用业务比较强的命名不合适，如果这里以后不是sale，就不合适了
-  static getHomeLocationA(callback){
-    Http.request({
+  static async getHomeLocationA() {
+    return await Http.request({
       url: `theme/by/names`,
       data: {
-        names: 't-1'
-      },
-      callback:data=>{
-        callback(data)
+        names: `${Theme.locationA}`
       }
     })
   }
