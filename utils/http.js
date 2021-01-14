300c42eee79cc5448a7597bb7587cd54a626a21b
vvboy <1285348378@qq.com>
2021-01-14 16:59:14 +0800

    第一周代码提交

--- utils/http.js
+++ utils/http.js
@@ -4,8 +4,8 @@ import {promisic} from "../miniprogram_npm/lin-ui/utils/util"
 class Http {
 
   // 函数加上async一定返回promise,哪怕是返回123也会封装成promise
-  // 异步函数前面加上await,函数内部出现await，这个函数必须加上async
-  // 使用await前提是这个函数调用返回结果,就算返回的不是promise
+  // 函数内部出现await，这个函数必须加上async
+  // 异步函数前面加上await,使用await前提是这个函数调用返回结果,就算返回的不是promise
   // 但是如果返回的不是promise，加上await没有意义
   static async request({
                          url,
